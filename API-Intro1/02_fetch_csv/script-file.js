60c0f4a2748cb23adc1bc12b3bd038ff87d67dd3
Antonio Ascue <ascuecode@gmail.com>
2020-02-04 01:36:54 +0100

    switching to line chart & styling

--- API-Intro1/02_fetch_csv/script-file.js
+++ API-Intro1/02_fetch_csv/script-file.js
@@ -6,22 +6,30 @@ async function chartIt() {
   await getData();
   const ctx = document.getElementById('chart').getContext('2d');
   const myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
       labels: xlabels,
       datasets: [{
         label: 'Global Average Temperature',
         data: ytemps,
-        backgroundColor: ['rgba(255, 99, 132, 0.2)'],
-        borderColor: ['rgba(255, 99, 132, 1)'],
-        borderWidth: 1
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        pointBackgroundColor:'#FF0000',
+        borderColor: '#F08080',
+        borderWidth: 1,
+        fill: false,
+        pointBorderWidth:1,
+        pointHoverBorderColor: '#0000FF',
+        pointHoverBorderWidth:3.8,
+        pointHoverRadius: 1.5,
       }]
     },
     options: {
       scales: {
         yAxes: [{
           ticks: {
-            beginAtZero: true
+            suggestedMin:13.4,
+            suggestedMax:15.2,
+            beginAtZero: false
           }
         }]
       }
@@ -39,7 +47,7 @@ async function getData(){
     table.forEach(row => {
       const yearTemp = row.split(',').slice(0, 2);
       xlabels.push(yearTemp[0]);
-      ytemps.push(yearTemp[1]);
+      ytemps.push(parseFloat(yearTemp[1]) + 14);
     });
   //comma-separated values and exclude all except the 2 first value
 }
