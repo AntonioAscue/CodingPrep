adc32020ed53092c0ee66bfb244ca3b5974e0fc2
Antonio Ascue <ascuecode@gmail.com>
2020-02-04 13:50:27 +0100

    refactoring code and styling chart

--- API-Intro1/02_fetch_csv/script-file.js
+++ API-Intro1/02_fetch_csv/script-file.js
@@ -1,25 +1,23 @@
-const xlabels =[];
-const ytemps= [];
 chartIt()
 
 async function chartIt() {
-  await getData();
+  const tempData = await getData();
   const ctx = document.getElementById('chart').getContext('2d');
   const myChart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: xlabels,
+      labels: tempData.years,
       datasets: [{
-        label: 'Global Average Temperature',
-        data: ytemps,
+        label: 'Combined Land-Surface Air and Sea-Surface Water Temperature Anomalies C°',
+        data: tempData.temp,
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        pointBackgroundColor:'#FF0000',
+        pointBackgroundColor: 'rgba(255, 99, 132, 0.2)',
         borderColor: '#F08080',
-        borderWidth: 1,
+        borderWidth: 0-5,
         fill: false,
-        pointBorderWidth:1,
-        pointHoverBorderColor: '#0000FF',
-        pointHoverBorderWidth:3.8,
+        pointBorderWidth: 1,
+        pointHoverBorderColor: '#FF0000',
+        pointHoverBorderWidth: 3.8,
         pointHoverRadius: 1.5,
       }]
     },
@@ -27,27 +25,35 @@ async function chartIt() {
       scales: {
         yAxes: [{
           ticks: {
-            suggestedMin:13.4,
-            suggestedMax:15.2,
-            beginAtZero: false
+            suggestedMin: 13.4,
+            suggestedMax: 15.2,
+            beginAtZero: false,
+            callback: function(value, index, values) {
+              return value + '°';
+          }
           }
         }]
       }
     }
   });
 }
-async function getData(){
+async function getData() {
+  const xlabels = [];
+  const ytemps = [];
   const response = await fetch('ZonAnn.Ts+dSST.csv');
   const data = await response.text();
-  const table = data
+  data
     .trim() // removes white space
     .split(/\n/) // splits text data based on line breaks
-    .slice(1); // removes first element === first row
-    
-    table.forEach(row => {
-      const yearTemp = row.split(',').slice(0, 2);
+    .slice(1) // removes first element containing string labels === first row
+    .forEach(row => {
+      const yearTemp = row.split(','); //comma-separated values
       xlabels.push(yearTemp[0]);
       ytemps.push(parseFloat(yearTemp[1]) + 14);
     });
-  //comma-separated values and exclude all except the 2 first value
+
+  return {
+    'years': xlabels,
+    'temp': ytemps
+  }
 }
