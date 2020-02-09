1e8afe1100493d9d512bfe4458c194a9f23cb8fb
Antonio Ascue <ascuecode@gmail.com>
2020-02-10 00:15:49 +0100

    update

--- API-Intro1/02a_fetch_mult_CSV/script.js
+++ API-Intro1/02a_fetch_mult_CSV/script.js
@@ -6,27 +6,29 @@ async function chart() {
     data: {
       labels: data.years,
       datasets: [{
-        label: 'Global Mean Temperature',
-        data: data.global,
+        label: 'Global',
+        data: data.globalTemp,
         backgroundColor: 'transparent',
         borderColor: 'rgba(255, 99, 132, 1)',
-        borderWidth: 1
+        borderWidth: 1,
+        pointRadius: 2
       },
       {
         label: 'Northern Hemisphere',
-        data: data.northern,
+        data: data.northernTemp,
         backgroundColor: 'transparent',
         borderColor: 'blue',
-        borderWidth: 1
+        borderWidth: 1,
+        pointRadius: 2
       },
       {
         label: 'Southern Hemisphere',
-        data: data.southern,
+        data: data.southernTemp,
         backgroundColor: 'transparent',
         borderColor: 'green',
-        borderWidth: 1
-      }
-      ]
+        borderWidth: 1,
+        pointRadius: 2
+      }]
     },
     options: {
       scales: {
@@ -36,8 +38,8 @@ async function chart() {
             min: 13.2,
             max: 15.6,
             stepSize: 0.2,
-            fontSize: 11,
-            callback: function(value) {
+            fontSize: 10,
+            callback: function (value) {
               return value + '°C';
             }
           }
@@ -50,8 +52,8 @@ async function chart() {
 async function getData() {
   const years = [];
   const globalTemp = [];
-  const NHTemp = [];
-  const SHTemp = [];
+  const northernTemp = [];
+  const southernTemp = [];
 
   const response = await fetch('./data/ZonAnn.Ts+dSST.csv');
   const table = await response.text();
@@ -62,16 +64,12 @@ async function getData() {
     .map(row => {
       const dataPoints = row.split(',');
       years.push(dataPoints[0]);
-      globalTemp.push(parseFloat(dataPoints[1] + 14) + 14);
-      NHTemp.push(parseFloat(dataPoints[2]) + 14);
-      SHTemp.push(parseFloat(dataPoints[3]) + 14);
+      globalTemp.push(parseFloat(dataPoints[1]) + 14);
+      northernTemp.push(parseFloat(dataPoints[2]) + 14);
+      southernTemp.push(parseFloat(dataPoints[3]) + 14);
     })
-  return {
-   'years': years,
-   'global': globalTemp,
-   'northern': NHTemp,
-   'southern': SHTemp
-  }
+    console.table(years, globalTemp)
+  return { years, globalTemp, northernTemp, southernTemp };
 }
 
 chart();
