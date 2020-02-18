4c67be04c2f2a761a52187ea52494122699f6d6e
Antonio Ascue <ascuecode@gmail.com>
2020-02-18 18:00:21 +0100

    setView for the first ISS-coordinates

--- API-Intro1/03_fetch_json/scriptFile.js
+++ API-Intro1/03_fetch_json/scriptFile.js
@@ -1,13 +1,11 @@
-const mymap = L.map('issMap').setView([0, 0], 1);
+const mymap = L.map('issMap').setView([0, 0], 2);
 const attribution =
 '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
-
 const tile_url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-//tile_url is not an actual URL, but it is a format for other particular url 
-//for tiles with a specific style(s), zoom(z), and coordinates(x,y)
 const tiles = L.tileLayer(tile_url,{attribution});
 tiles.addTo(mymap);
+//tile_url is not an actual URL, but it is a format for other particular url 
+//for tiles with a specific style(s), zoom(z), and coordinates(x,y)
 
 const myIcon = L.icon({
   iconUrl: 'ISS.png',
@@ -16,16 +14,19 @@ const myIcon = L.icon({
 });
 const marker = L.marker([0, 0], {icon: myIcon}).addTo(mymap);
 
-const coordISS = async () => {
+const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+
+let firstView = true;
+async function coordISS() {
   const response = await fetch(api_url);
   const ISS_data = await response.json()
-  const {latitude, longitude} = ISS_data;
-  marker.setLatLng([latitude, longitude]) 
-  console.log(latitude);
-  console.log(longitude);
-  document.getElementById('lat').innerHTML = latitude;
-  document.getElementById('lon').innerHTML = longitude;
-}
- 
-const intervalID = setInterval(coordISS, 3000)
+  const {latitude, longitude} = ISS_data; // destructuring
 
+  marker.setLatLng([latitude, longitude]); // L.marker longitude and latitude
+
+  firstView?( mymap.setView([latitude,longitude]), firstView = false):
+  document.getElementById('lat').innerHTML = latitude.toFixed(2);
+  document.getElementById('lon').innerHTML = longitude.toFixed(2);
+}
+coordISS()
+setInterval(coordISS, 3000)
