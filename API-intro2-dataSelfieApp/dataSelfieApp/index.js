0e36f3b4e540bdaca2c2b06b8317fbba5ef7dffc
Antonio Ascue <ascuecode@gmail.com>
2020-02-18 23:46:45 +0100

    local server host index.html

---
+++ API-intro2-dataSelfieApp/dataSelfieApp/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+app.listen(3000, () => {
+  console.log('listening at 3000 -port');
+});
+app.use(express.static('public')); //express host static file
