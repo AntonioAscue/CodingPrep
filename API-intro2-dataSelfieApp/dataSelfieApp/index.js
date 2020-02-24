cc5779ac5dcd98be04410fc1dbdcb8cde0c67ab0
Antonio Ascue <ascuecode@gmail.com>
2020-02-24 17:10:47 +0100

    Routing and fetch POST the coordinates

--- API-intro2-dataSelfieApp/dataSelfieApp/index.js
+++ API-intro2-dataSelfieApp/dataSelfieApp/index.js
@@ -1,6 +1,18 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 app.listen(3000, () => {
   console.log('listening at 3000 -port');
 });
 app.use(express.static('public')); //express host static file
+app.use(express.json()); // support json encoded bodies
+
+app.post('/api', (req, res) => {
+  console.log(req.body);
+  const data = req.body;
+  res.json({
+    status: 'Success!',
+    latitude: data.lat,
+    longitude: data.lon
+  });
+});
--- API-intro2-dataSelfieApp/dataSelfieApp/public/index.js
+++ API-intro2-dataSelfieApp/dataSelfieApp/public/index.js
@@ -1,9 +1,24 @@
 if ("geolocation" in navigator) {
   console.log('geolocation works');
-  navigator.geolocation.getCurrentPosition(position =>{
+  navigator.geolocation.getCurrentPosition(position => {
     const postData = position.coords;
-    document.getElementById('latitude').textContent = postData.latitude;
-    document.getElementById('longitude').textContent = postData.longitude;
+    const lat = postData.latitude.toFixed(2)
+    const lon = postData.longitude.toFixed(2)
+    document.getElementById('latitude').textContent = lat;
+    document.getElementById('longitude').textContent = lon;
+
+    const data = { lat, lon };
+    const options  =  {
+      method:'POST', 
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(data)
+    }
+    fetch('/api', options)
+    .then(res => res.json())
+    .then(json => console.log(json));
   });
 } else {
   console.log("geolocation doesn/'t work");
