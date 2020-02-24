0815fd02c0c3902b0928185e4f5610efdf5a90c7
Antonio Ascue <ascuecode@gmail.com>
2020-02-24 03:59:21 +0100

    Add and remove leaflet  map and marker with button-click event

--- API-intro2-dataSelfieApp/exercise03_GeoLocMap/public/index.js
+++ API-intro2-dataSelfieApp/exercise03_GeoLocMap/public/index.js
@@ -1,21 +1,39 @@
+const geoMap = new L.map('mapid')
+const attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const openStreetMapURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?';
+const tileLayer = L.tileLayer(openStreetMapURL, { attribution });
+const button = document.getElementById("geolocation");
+
 if ("geolocation" in navigator) {
   navigator.geolocation.getCurrentPosition(position => {
-    const latitude = position.coords.latitude.toFixed(2);
-    const longitude = position.coords.longitude.toFixed(2);
-    const button = document.getElementById("geolocation");
+    const lat = position.coords.latitude.toFixed(2);
+    const lon = position.coords.longitude.toFixed(2);
+    const theMarker = L.marker([lat, lon]);
 
     button.addEventListener('click', () => {
       button.innerText === "Geolocation" ? (button.innerText = "Hide location",
-        coordText(latitude, longitude))
+        coordToHTML(lat, lon),
+        addToMap(tileLayer, theMarker),
+        geoMap.setView([lat, lon], 13))
         : (button.innerText = "Geolocation",
-          coordText('', ''));
+          coordToHTML('', ''),
+          rmvFromMap(tileLayer, theMarker));
     });
 
-    function coordText(lat, lon) {
+    function addToMap(...layers) {
+      layers.forEach(x => x.addTo(geoMap));
+    }
+    
+    function rmvFromMap(...layers) {
+      layers.forEach(x => geoMap.removeLayer(x));
+    }
+    
+    function coordToHTML(lat, lon) {
       document.getElementById('latitude').innerText = lat;
       document.getElementById('longitude').innerText = lon;;
     }
   });
 } else {
-  console.log("geolocation sucks");
+  console.log('Geolocation does not function');
 }
+
