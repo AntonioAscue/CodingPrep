1c81e589bb8b428feda312eb4aa6bac6d5a8c1cd
Antonio Ascue <ascuecode@gmail.com>
2020-02-29 22:32:50 +0100

    append database on the DOM

--- API-intro2-dataSelfieApp/dataSelfieApp/public/all.js
+++ API-intro2-dataSelfieApp/dataSelfieApp/public/all.js
@@ -1,6 +1,19 @@
 async function getData() {
   const res = await fetch('/api');
   const data = await res.json();
-  console.log(data);
+
+  data.forEach(item => {
+    const rootDiv = document.createElement('ul');
+    const geoLocation = document.createElement('li');
+    const mood = document.createElement('li');
+    const ts = document.createElement('li');
+    const dateString = new Date(item.timestamp).toLocaleString();
+
+    geoLocation.textContent = `Coordinates: latitude: ${item.lat}, longitude ${item.lon}`;
+    mood.textContent = `Mood: ${item.mood}`;
+    ts.textContent = `Timestamp: ${dateString}`;
+    rootDiv.append(geoLocation, mood, ts);
+    document.body.append(rootDiv);
+  });
 }
 getData()
