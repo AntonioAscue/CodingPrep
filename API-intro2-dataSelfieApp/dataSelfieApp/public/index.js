ec7e1dfb9cf3e072b28a276711bfb7148b1ef50e
Antonio Ascue <ascuecode@gmail.com>
2020-03-02 15:17:16 +0100

    p5-video canvas and home-database buttons

--- API-intro2-dataSelfieApp/dataSelfieApp/public/index.js
+++ API-intro2-dataSelfieApp/dataSelfieApp/public/index.js
@@ -1,31 +1,38 @@
+function setup() {
+  noCanvas();
+  const video =  createCapture(VIDEO);
+  video.size(320,240)
 
-let lat;
-let lon;
-const button = document.getElementById('btn');
-button.addEventListener('click', () => {
-  const submitData = document.getElementById('submit').value;
-  const data = { lat, lon, mood: submitData };
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: JSON.stringify(data)
-  }
-  fetch('/api', options)
-    .then(res => res.json())
-    .then(json => console.log(json));
-}); 
-
-if ("geolocation" in navigator) {
-  navigator.geolocation.getCurrentPosition(position => {
-    const postData = position.coords;
-    lat = postData.latitude.toFixed(2);
-    lon = postData.longitude.toFixed(2);
-    document.getElementById('latitude').textContent = lat;
-    document.getElementById('longitude').textContent = lon;
+  let lat, lon;
+  const button = document.getElementById('btn');
+  button.addEventListener('click', () => {
+    const submitData = document.getElementById('submit').value;
+    const data = { lat, lon, mood: submitData };
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(data)
+    }
+    fetch('/api', options)
+      .then(res => res.json())
+      .then(json => console.log(json));
   });
-} else {
-  console.log("geolocation doesn/'t work");
+
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(position => {
+      const postData = position.coords;
+      lat = postData.latitude.toFixed(2);
+      lon = postData.longitude.toFixed(2);
+      document.getElementById('latitude').textContent = lat;
+      document.getElementById('longitude').textContent = lon;
+    });
+  } else {
+    console.log("geolocation doesn/'t work");
+    
+  }
+
 }
+
