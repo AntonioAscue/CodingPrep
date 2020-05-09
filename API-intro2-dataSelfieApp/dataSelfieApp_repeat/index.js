c588f71d8b30d86b6c11ced18856525686f0312d
Antonio Ascue <ascuecode@gmail.com>
2020-05-09 22:56:32 +0200

    leaflet map & click event

--- API-intro2-dataSelfieApp/dataSelfieApp_repeat/index.js
+++ API-intro2-dataSelfieApp/dataSelfieApp_repeat/index.js
@@ -2,3 +2,7 @@ const express =  require('express');
 const app =  express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public')); //host static files in a public
+
+app.post('/', (req, res) => {
+  console.log(req);
+})
--- API-intro2-dataSelfieApp/dataSelfieApp_repeat/public/index.js
+++ API-intro2-dataSelfieApp/dataSelfieApp_repeat/public/index.js
@@ -3,19 +3,46 @@ goodbyeLink.addEventListener('click', () => {
   goodbyeLink.href = "http://localhost:3000/goodbye.html"
 })
 
-if('geolocation' in navigator){
+if ('geolocation' in navigator) {
   console.log('navigator exists');
   navigator.geolocation.getCurrentPosition(position => {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
+    const lat = position.coords.latitude.toFixed(2);
+    const lon = position.coords.longitude.toFixed(2);
     const ts = position.timestamp;
-    
     const latDom = document.getElementById("latitude");
     const lonDom = document.getElementById("longitude");
-    latDom.innerHTML = lat;
-    lonDom.innerHTML = lon;
+
+    const coordBtn = document.getElementsByTagName("button")[0];
+    coordBtn.addEventListener('click', () => {
+
+      latDom.innerHTML = lat;
+      lonDom.innerHTML = lon;
+      mapCoord(lat, lon);
+      const data 
+    });
+    fetch('/', {
+      method: 'POST',
+      Headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
   })
 }
 else {
 
+}
+
+
+function mapCoord(x, y) {
+  const mymap = L.map('mapid').setView([x, y], 13);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(mymap);
+  marker(x, y, mymap)
+}
+
+function marker(x,y, map){
+  L.marker([x, y]).addTo(map)
+    .openPopup();
 }
